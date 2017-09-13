436e4ac8367cb8c9165ff0103052207d8f2a1e0e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-13 18:54:41 -0400

    JS Script: We can now move to next page.

--- injectedScript.js
+++ injectedScript.js
@@ -1,11 +1,19 @@
-leaderboardChange("3", "102", 2, "0", 0, "Mono");
+var WAIT_DELAY = 1000;
 
 function toArray(nodeList) { return Array.prototype.slice.call(nodeList); }
+
+function nextPage(callback) {
+    toArray(document.querySelectorAll(".leaderboard_table_page_list a")).slice(-1)[0].click();
+    setTimeout(callback, WAIT_DELAY);
+}
 
 var entries = toArray(document.querySelectorAll(".leaderboard_main_table"));
 function getStuff() {
     if (!entries.length) {
-        return;
+        nextPage(function() {
+            entries = toArray(document.querySelectorAll(".leaderboard_main_table"));
+            getStuff();
+        });
     } 
     var entry = entries.shift();
     entry.querySelector(".leaderboard_details_cell a").click();
@@ -13,15 +21,19 @@ function getStuff() {
     setTimeout(function() {
         var stats = document.querySelector("#sb-content").contentDocument
             .querySelector(".user_stats_table");
-        var gameData = toArray(stats.firstChild.children).reduce(function(accum, element) {
-            var th = element.getElementsByTagName("th")[0];
-            var td = element.getElementsByTagName("td")[0];
-            accum[th.innerText] = td.innerText;
-            return accum;
-        }, {});
-        console.log(gameData);
+        if (stats != null) {
+            var gameData = toArray(stats.firstChild.children).reduce(function(accum, element) {
+                var th = element.getElementsByTagName("th")[0];
+                var td = element.getElementsByTagName("td")[0];
+                accum[th.innerText] = td.innerText;
+                return accum;
+            }, {});
+            console.log(gameData);
+        }
         Shadowbox.close();
-        setTimeout(getStuff, 1000);
-    }, 1000);
+        setTimeout(getStuff, WAIT_DELAY);
+    }, WAIT_DELAY);
 }
-getStuff();
+
+leaderboardChange("3", "102", 2, "0", 0, "Mono");
+setTimeout(getStuff, WAIT_DELAY);
