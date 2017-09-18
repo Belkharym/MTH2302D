98ee8928d245e9795085ca47378ab8a2ad157ec9
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-18 12:08:28 -0400

    More progress on the injectedScript.

--- injectedScript.js
+++ injectedScript.js
@@ -2,38 +2,66 @@ var WAIT_DELAY = 1000;
 
 function toArray(nodeList) { return Array.prototype.slice.call(nodeList); }
 
-function nextPage(callback) {
+function waitTill(condObj, callback) {
+    if (!condObj.val) {
+        setTimeout(waitTill, WAIT_DELAY, condObj);
+    }
+    else {
+        callback();
+    }
+}
+
+function nextPage() {
+    console.log("nextPage-ing");
     toArray(document.querySelectorAll(".leaderboard_table_page_list a")).slice(-1)[0].click();
-    setTimeout(callback, WAIT_DELAY);
+    setTimeout(function() {
+                   entries = toArray(document.querySelectorAll(".leaderboard_main_table"));
+                   console.log("entries: ", entries);
+                   getStuff();
+               },
+               2 * WAIT_DELAY);
 }
 
 var entries = toArray(document.querySelectorAll(".leaderboard_main_table"));
-function getStuff() {
-    if (!entries.length) {
-        nextPage(function() {
-            entries = toArray(document.querySelectorAll(".leaderboard_main_table"));
-            getStuff();
-        });
-    } 
-    var entry = entries.shift();
-    entry.querySelector(".leaderboard_details_cell a").click();
 
-    setTimeout(function() {
-        var stats = document.querySelector("#sb-content").contentDocument
-            .querySelector(".user_stats_table");
-        if (stats != null) {
-            var gameData = toArray(stats.firstChild.children).reduce(function(accum, element) {
-                var th = element.getElementsByTagName("th")[0];
-                var td = element.getElementsByTagName("td")[0];
-                accum[th.innerText] = td.innerText;
-                return accum;
-            }, {});
-            console.log(gameData);
-        }
-        Shadowbox.close();
-        setTimeout(getStuff, WAIT_DELAY);
-    }, WAIT_DELAY);
+function getStuff() {
+    console.log("Next iteration.");
+    console.log("Curr length: ", entries.length);
+
+    if (entries.length === 0) {
+        setTimeout(nextPage, 2 * WAIT_DELAY);
+    }
+    else {
+        getNext();
+    }
+
 }
 
-leaderboardChange("3", "102", 2, "0", 0, "Mono");
-setTimeout(getStuff, WAIT_DELAY);
+function getNext() {
+    var entry = entries.shift();
+    entry.querySelector(".leaderboard_details_cell a").click();
+    console.log("entry: ", entry);
+
+    setTimeout(extractStatsFromShadowbox, WAIT_DELAY);
+}
+
+function extractStatsFromShadowbox() {
+    var stats = document.querySelector("#sb-content").contentDocument
+        .querySelector(".user_stats_table");
+    if (stats != null) {
+        var gameData = toArray(stats.firstChild.children).reduce(function(accum, element) {
+            var th = element.getElementsByTagName("th")[0];
+            var td = element.getElementsByTagName("td")[0];
+            accum[th.innerText] = td.innerText;
+            return accum;
+        }, {});
+        console.log("Data:", gameData);
+    }
+    else {
+        console.log("stats were null");
+    }
+    Shadowbox.close();
+    setTimeout(getStuff, WAIT_DELAY);
+}
+
+getStuff();
